7f5aa31064bd48dd73cb192ef0127329d2a0d44d
mmd-yavarii <mdyavarii@gmail.com>
2026-09-21 12:10:20 +0330

    create get_absolte_url for generate url for object models

--- blog/models/Post.py
+++ blog/models/Post.py
@@ -1,6 +1,8 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 from uuid import uuid4
 
 
@@ -40,6 +42,9 @@ class Post (models.Model):
 
     def __str__ (self):
         return self.title
+
+    def get_absolute_url (self):
+        return reverse("blog:post_details" , kwargs={"id" : self.id})
 
     @property
     def likes_count (self):
